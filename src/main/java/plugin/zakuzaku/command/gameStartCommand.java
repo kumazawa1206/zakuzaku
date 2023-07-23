dac0c702f1f76e16aa2a2de540485abee3e7eab3
yu ji <leon5617@gmail.com>
2023-07-23 23:08:51 +0900

    command実行時に装備が変更される

--- src/main/java/plugin/zakuzaku/command/gameStartCommand.java
+++ src/main/java/plugin/zakuzaku/command/gameStartCommand.java
@@ -3,7 +3,7 @@ package plugin.zakuzaku.command;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.SplittableRandom;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Block;
@@ -11,35 +11,42 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 
 public class gameStartCommand implements CommandExecutor {
 
-  private final int stackAmount = 27;
   private Block playerBlock;
 
   @Override
   public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
     if (sender instanceof Player player) {
       World world = player.getWorld();
+      PlayerInventory inventory = player.getInventory();
+      inventory.setHelmet(new ItemStack(Material.DIAMOND_HELMET));
+      inventory.setChestplate(new ItemStack(Material.DIAMOND_CHESTPLATE));
+      inventory.setLeggings(new ItemStack(Material.DIAMOND_LEGGINGS));
+      inventory.setBoots(new ItemStack(Material.DIAMOND_BOOTS));
+      inventory.setItemInMainHand(new ItemStack(Material.DIAMOND_PICKAXE));
 
       List<Material> blocksList = new ArrayList<>();
       blocksList.add(Material.STONE);
       blocksList.add(Material.DIAMOND_ORE);
       blocksList.add(Material.LAPIS_ORE);
 
-      int random = ThreadLocalRandom.current().nextInt(blocksList.size());
+      int random = new SplittableRandom().nextInt(blocksList.size());
 
-      for (int i = 0; i < 52; i++) {
+      for (int i = 0; i < 66; i++) {
         blocksList.add(Material.STONE);
       }
-      for (int i = 0; i < 10; i++) {
+      for (int i = 0; i < 3; i++) {
         blocksList.add(Material.DIAMOND_ORE);
       }
-      for (int i = 0; i < 10; i++) {
+      for (int i = 0; i < 3; i++) {
         blocksList.add(Material.LAPIS_ORE);
       }
 
-      Collections.shuffle(blocksList, ThreadLocalRandom.current());
+      Collections.shuffle(blocksList);
 
       int count = 0;
       playerBlock = player.getLocation().subtract(10, 0, 0).getBlock();
@@ -51,7 +58,6 @@ public class gameStartCommand implements CommandExecutor {
             if (count >= 72) {
               break;
             }
-
             Material blockType = blocksList.get(count);
             world.getBlockAt(x, y, z).setType(blockType);
             count++;
