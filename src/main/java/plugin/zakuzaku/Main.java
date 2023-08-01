f496435b6980bb1dd145e9d30a197590964c8307
yu ji <leon5617@gmail.com>
2023-08-02 06:16:23 +0900

    制限時間を設定し、制限時間になるとメッセージが送信されるようにしました。 制限時間になった時に、ブロックが消える仕様必要。

--- src/main/java/plugin/zakuzaku/Main.java
+++ src/main/java/plugin/zakuzaku/Main.java
@@ -1,15 +1,14 @@
 package plugin.zakuzaku;
 
 import org.bukkit.Bukkit;
-import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import plugin.zakuzaku.command.GameStartCommand;
 
-public final class Main extends JavaPlugin implements Listener {
+public final class Main extends JavaPlugin {
 
   @Override
   public void onEnable() {
-    GameStartCommand gameStartCommand = new GameStartCommand();
+    GameStartCommand gameStartCommand = new GameStartCommand(this);
     Bukkit.getPluginManager().registerEvents(gameStartCommand, this);
     getCommand("gameStart").setExecutor(gameStartCommand);
   }
--- src/main/java/plugin/zakuzaku/command/GameStartCommand.java
+++ src/main/java/plugin/zakuzaku/command/GameStartCommand.java
@@ -4,11 +4,10 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
-import java.util.SplittableRandom;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
-import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -18,29 +17,47 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
+import plugin.zakuzaku.Main;
+import plugin.zakuzaku.data.PlayerScore;
 
 public class GameStartCommand implements CommandExecutor, Listener {
 
-  private Block playerBlock;
+  private Main main;
   private List<Material> allowedBlocks;
-  private Player player;
-  private int score;
+  private List<PlayerScore> playerScoreList = new ArrayList<>();
+  private int gameTime = 15;
 
-  public GameStartCommand() {
-    allowedBlocks = List.of(Material.REDSTONE, Material.BLACKSTONE,
+  public GameStartCommand(Main main) {
+    this.main = main;
+
+    allowedBlocks = List.of(Material.STONE, Material.BLACKSTONE,
         Material.DIAMOND_ORE,
         Material.LAPIS_ORE);
   }
 
   @Override
   public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
     if (sender instanceof Player player) {
-      this.player = player;
+      if (playerScoreList.isEmpty()) {
+        addNewPlayer(player);
+      } else {
+        for (PlayerScore playerScore : playerScoreList) {
+          if (!playerScore.getPlayerName().equals(player.getName())) {
+          }
+        }
+      }
+
+      gameTime = 15;
       World world = player.getWorld();
       PlayerInventory inventory = player.getInventory();
       inventory.setItemInMainHand(new ItemStack(Material.DIAMOND_PICKAXE));
 
-      //Listに持たせたMaterialをランダムに並べる。
+      Bukkit.getScheduler().runTaskLater(main, () -> {
+        player.sendMessage("ゲーム終了！お疲れさまでした。");
+      }, 15 * 20);
+
+      //Listに持たせたMaterialの任意の数を指定。。
       List<Material> blocksList = new ArrayList<>();
       blocksList.addAll(Collections.nCopies(40, Material.STONE));
       blocksList.addAll(Collections.nCopies(3, Material.DIAMOND_ORE));
@@ -49,22 +66,19 @@ public class GameStartCommand implements CommandExecutor, Listener {
       blocksList.addAll(Collections.nCopies(40, Material.BLACKSTONE));
       Collections.shuffle(blocksList);
 
+      //ランダムに並べたMaterialの出現場所
       int count = 0;
       Location playerLocation = player.getLocation();
-      int offsetX = 4;
-      int offsetY = 3;
-      int offsetZ = 4;
-      int distance = 3;
-
-      for (int x = playerLocation.getBlockX() - offsetX; x <= playerLocation.getBlockX() + offsetX;
-          x++) {
-        for (int y = playerLocation.getBlockY() - offsetY;
-            y <= playerLocation.getBlockY() + offsetY; y++) {
-          for (int z = playerLocation.getBlockZ() - offsetZ;
-              z <= playerLocation.getBlockZ() + offsetZ; z++) {
+      //ランダムに並べたMaterialの出現場所の座標
+      for (int x = playerLocation.getBlockX() - 4;
+          x <= playerLocation.getBlockX() + 4; x++) {
+        for (int y = playerLocation.getBlockY() - 3;
+            y <= playerLocation.getBlockY() + 3; y++) {
+          for (int z = playerLocation.getBlockZ() - 4;
+              z <= playerLocation.getBlockZ() + 4; z++) {
 
             Location blockLocation = new Location(world, x, y, z);
-            if (blockLocation.distance(playerLocation) > distance) {
+            if (blockLocation.distance(playerLocation) > 3) {
               if (!blockLocation.getBlock().getType().isSolid()) {
                 if (count >= 126) {
                   break;
@@ -81,31 +95,35 @@ public class GameStartCommand implements CommandExecutor, Listener {
     return true;
   }
 
-  private Material getRandomBlockType() {
-    Material[] blockTypes = {Material.DIAMOND_ORE, Material.STONE, Material.LAPIS_ORE};
-    int random = new SplittableRandom().nextInt(blockTypes.length);
-    return blockTypes[random];
-  }
-
   @EventHandler
   public void onBlockBreak(BlockBreakEvent e) {
     Player player = e.getPlayer();
-    if (Objects.isNull(player)) {
-      return;
-    }
-    if (Objects.isNull(this.player)) {
+    if (Objects.isNull(player) || playerScoreList.isEmpty()) {
       return;
     }
 
-    if (this.player.getName().equals(player.getName())) {
-      score += 10;
-      player.sendMessage("採掘しました！現在のスコアは" + score + "点です。");
+    for (PlayerScore playerScore : playerScoreList) {
+      if (playerScore.getPlayerName().equals(player.getName())) {
+        playerScore.setScore(playerScore.getScore() + 10);
+        player.sendMessage("採掘しました！現在のスコアは" + playerScore.getScore() + "点です。");
+      }
     }
+    //gameStart実行時に生成されたブロック以外は採掘できなくなる
     Material brokenBlockType = e.getBlock().getType();
     if (!allowedBlocks.contains(brokenBlockType)) {
       e.setCancelled(true);
     }
+  }
 
+
+  /**
+   * 新規のプレイヤー情報をリストに追加します。
+   *
+   * @param player コマンドを実行したプレイヤー
+   */
+  private void addNewPlayer(Player player) {
+    PlayerScore newPlayer = new PlayerScore();
+    newPlayer.setPlayerName(player.getName());
+    playerScoreList.add(newPlayer);
   }
 }
-
